592c211120ab0629bfb8092f8254c8f09b06366b
vijaypuohit322 <vijaypurohit322@gmail.com>
2021-01-22 12:47:05 +0530

    adding views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ecommerce_report_page(request):
+   return render(request, 'retail_report.html', {})
+
+
+def marketing_report_page(request):
+   return render(request, 'marketing_report.html', {})
